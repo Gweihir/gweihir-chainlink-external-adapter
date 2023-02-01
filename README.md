8d68ef68147ebc4a44b0937acb8670b85269bc05
Spencer Brown <git@spencerwebservices.com>
2023-02-01 14:42:49 -0800

    Set up truffle; Add example contract with scripts (#8669ecvhe)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gweihir-chainlink-external-adapter
 Services Ethereum smart contracts with Kusama data via Chainlink
+
+## Appreciation
+This project is influenced by https://github.com/aelmanaa/chainlink-local-kubernetes and 
+by [Chainlink's Truffle box](https://github.com/smartcontractkit/truffle-starter-kit):
+```
+truffle unbox smartcontractkit/box
+```
---
+++ migrations/1_deploy.js
@@ -0,0 +1,39 @@
+// truffle migrate --f 1 --to 1 --network ganache
+
+const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
+const { Operator } = require('@chainlink/contracts/truffle/v0.7/Operator')
+const Consumer = artifacts.require('Consumer')
+const conf = require('../config/addr.json')
+const fs = require('fs')
+
+module.exports = async (deployer, network, [owner]) => {
+  // Local (development) networks need their own deployment of the LINK
+  // token and the Operator contract
+  LinkToken.setProvider(deployer.provider)
+  Operator.setProvider(deployer.provider)
+  try {
+    await deployer.deploy(LinkToken, { from: owner })
+    await deployer.deploy(Operator, LinkToken.address, owner, { from: owner })
+    await deployer.deploy(Consumer, LinkToken.address, { from: owner })
+
+    const addr = {
+      linkTokenAddress: LinkToken.address,
+      operatorAddress: Operator.address,
+      consumerAddress: Consumer.address,
+    }
+
+    console.log(`Link address is ${LinkToken.address}`)
+    console.log(`Operator address is ${Operator.address}`)
+    console.log(`Consumer address is ${Consumer.address}`)
+
+    const newConf = conf
+    newConf[network] = addr
+    fs.writeFileSync(
+      __dirname + '/../config/addr.json',
+      JSON.stringify(newConf, null, '\t'),
+      'utf-8'
+    )
+  } catch (err) {
+    console.error(err)
+  }
+}
