40aeb63c6f0eaff6d9d05a8d58160330bb3d905c
Spencer Brown <git@spencerwebservices.com>
2023-03-20 13:06:27 -0700

    Set up testing framework (#862jfq722)
    Added more KusamaAPI tests

--- src/services/kusama.test.ts
+++ src/services/kusama.test.ts
@@ -2,12 +2,26 @@ import { KusamaAPI } from './kusama'
 
 const address = 'Gk2FdnhLNnumXWrxjyk7yQstq3CL1Ni4QNCPuTeSjCdDoMh'
 
-const testGetAccountBalance = async () => {
-  const response = await KusamaAPI.getAccountBalance({ address })
-  console.log(response)
-
+afterAll(async () => {
   const api = await KusamaAPI.getApi()
-  api.disconnect()
-}
+  await api.disconnect()
+})
 
-testGetAccountBalance()
+test("get account's latest balance", async () => {
+  const response = await KusamaAPI.getAccountBalance({ address })
+  expect(() => BigInt(response)).not.toThrow()
+})
+
+test("get account's balance at specific block hash", async () => {
+  const blockHash = '0xfd20e7e096c6e988ef59e2b2f9940aed610afc8768cb885dec54ee9a39d8995a'
+  const response = await KusamaAPI.getAccountBalance({ address, blockHash })
+  expect(() => BigInt(response)).not.toThrow()
+  expect(response).toBe('37973447220861534')
+})
+
+test("get account's balance at specific block number", async () => {
+  const blockNumber = 16698003
+  const response = await KusamaAPI.getAccountBalance({ address, blockNumber })
+  expect(() => BigInt(response)).not.toThrow()
+  expect(response).toBe('37973447220861534')
+})
