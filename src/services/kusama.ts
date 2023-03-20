8f19153cb8ee3fa5e84006131f2f96bd136e593f
Spencer Brown <git@spencerwebservices.com>
2023-03-20 13:03:06 -0700

    Allow block number as input (#3w3bayt)

--- src/services/kusama.ts
+++ src/services/kusama.ts
@@ -17,26 +17,40 @@ export class KusamaAPI {
   }
 
   /**
-   * Get the account balance in Planck for a specific address
+   * Get the account balance in Planck for a specific address.
+   * If both `blockHash` and `blockNumber` are provided, then `blockHash` will be used and `blockNumber` will be ignored.
    * @param {string} input.address - Kusama account address (in SS58 format)
    *   See more here: https://guide.kusama.network/docs/learn-account-advanced
+   * @param {string | undefined} input.blockHash - Block hash of block to be queried for account balance
+   * @param {number | undefined} input.blockNumber - Block number of block to be queried for account balance
    * @returns {Promise<string>} - Planck amount
    */
   public static getAccountBalance = async ({
     address,
     blockHash,
+    blockNumber,
   }: {
     address: string
-    blockHash: string
+    blockHash?: string
+    blockNumber?: number
   }): Promise<string> => {
     const api = await KusamaAPI.getApi()
 
-    // TODO: Have block hash/number be optional
+    if (blockHash) {
+      // Get block and query that specific block
+      const block = await api.at(blockHash)
+      const result = await block.query.system.account(address)
+      return result.data.free.toString()
+    } else if (blockNumber !== undefined) {
+      // Lookup block hash, get block, and then query that specific block
+      const blockHash = await api.rpc.chain.getBlockHash(blockNumber)
+      const block = await api.at(blockHash)
+      const result = await block.query.system.account(address)
+      return result.data.free.toString()
+    }
 
-    const block = await api.at(blockHash)
-
-    const result = await block.query.system.account(address)
-
+    // Query latest block available
+    const result = await api.query.system.account(address)
     return result.data.free.toString()
   }
 }
